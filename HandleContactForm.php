cc1bdbedd81d52cdc19dc5f852aba02ae84c3c22
Mobinet113 <benjaminroffe@mobiusit.net>
2019-02-11 14:20:16 +0000

    Removed unnecessary dependencies

--- HandleContactForm.php
+++ HandleContactForm.php
@@ -3,8 +3,6 @@
  * Class to handle contact form enquiries
  */
 
-require_once('../config.php');
-require('../vendor/autoload.php');
 
 if (isset($_REQUEST)) {
   new HandleContactForm(file_get_contents("php://input"));
@@ -22,7 +20,7 @@ class HandleContactForm
     $this->errors = array();
 
     if ($this->validateEntry()) {
-      $this->sendNotifications();
+      // If the form is valid, then do something cool
     }
   }
 
@@ -95,61 +93,5 @@ class HandleContactForm
     return true;
   }
 
-  private function sendNotifications()
-  {
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("notifications@niikiis.com", "niikiis landing page");
-    $email->setSubject("New submission from the contact form");
-
-    // Define the recipients
-    foreach( NOTIFICATION_EMAIL as $notifAddress ) {
-      $email->addTo($notifAddress, "Admin");
-    }
-
-    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-
-    $email->addContent(
-      "text/html", "
-
-        <table>
-          <tr>
-            <td style='text-align: center;'>
-              <img src='https://www.nowcomms.com/wp-content/uploads/2019/01/logo-niikiis.png' alt='niikiis logo' width='75' />
-            </td>
-          </tr>
-          
-          <h1>
-            New submission from the contact form<br />
-            <small>Here are their details...</small>
-          </h1>" . $this->renderEmailTable() .
-      "</table>"
-    );
-
-
-    $sendgrid = new \SendGrid(SENDGRID_API_KEY);
-
-    try {
-      $response = $sendgrid->send($email);
-      print $response->statusCode() . "\n";
-      print $response->body() . "\n";
-    } catch (Exception $e) {
-      echo 'Caught exception: ' . $e->getMessage() . "\n";
-    }
-  }
-
-  private function renderEmailTable()
-  {
-
-    $html = "<table>";
-
-    foreach ($this->entries as $entry) {
-      $html .= "<tr><td><strong>" . strtoupper($entry['name']) . "</strong></td><td>" . $entry['value'] . "</td></tr>";
-    }
-
-    $html .= "</table>";
-
-    return $html;
-
-  }
 
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Form Validation
+A quick and simple example of some basic form validation for the front
+and backend.
